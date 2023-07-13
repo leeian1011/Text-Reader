c7ef0e47ba7c23de5ef77bd3712966b425807d0d
Lee <leeian1011@gmail.com>
2023-07-13 08:48:43 +0800

    io::fs(filesystem) library ??

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main(){
-    let mut test: File = File::open("../../src/text.txt").expect("err file");
+    let mut test: File = File::open("src/text.txt").expect("err file");
+    let mut s1: String = String::new();
+    
+    test.read_to_string(&mut s1).expect("could not read");
+
+    println!("{}", s1);
 }
