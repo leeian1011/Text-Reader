cdf6c9136633918352b2f7887561fd2438c46f74
Lee <leeian1011@gmail.com>
2023-07-14 22:29:49 +0800

    removed pub from setters

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ use std::io::prelude::*;
 fn main(){
     let s: String = String::from("src/files/test.txt");
     let mut test: CountData = CountData::new(&s);
-    test.set_char_count(100);
-    test.set_word_count(20);
-    test.set_sentence_count(2);
     println!("{:?}", test);
 }
 
